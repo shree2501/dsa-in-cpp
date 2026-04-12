bafb25d33c4aa95622261316621bb3d7892c33ad
Shree Sharma <sharmashree750@gmail.com>
2026-04-12 16:50:59 +0530

    rectangle

---
+++ Day4/Patterns/rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
