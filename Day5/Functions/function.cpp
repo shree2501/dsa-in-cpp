d3b3626b293cfcea202bb1589107a786dfdc64d5
Shree Sharma <sharmashree750@gmail.com>
2026-04-14 10:53:42 +0530

    Functions

---
+++ Day5/Functions/function.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+//without argument with return type
+// int sum(int a,int b);          //function declaration
+// int main()
+// {
+//     int a=4,b=3,c;
+//     c = sum(a,b);                   //function call
+//     cout<<"Sum is: "<< c;
+//     return 0;
+// }
+// int sum(int a,int b)      
+// {
+//     return a+b;               //function definition
+// }
+
+//without return value with argunment
+// void printstar(int n)
+// {
+//     for(int i=0;i<=n;i++)
+//     {
+//         cout << "*";
+//     }
+// }
+// int main()
+// {
+//     int n=4;
+//     printstar(7);
+//     return 0;
+// }
+
+//without argument with return value
+// int takevalue()
+// {
+//     int a;
+//     cout<<"enter the number: ";
+//     cin>>a;
+//     return a;
+// }
+// int main()
+// {   
+//     int num;
+//     num = takevalue();
+//     cout<<"the number is "<< num;
+//     return 0;
+// }
+
+//without argument without return value
+// void myfunction()
+// {
+//   cout<<"this is a function without argument and without return value";
+// }
+// int main()
+// {
+//     myfunction();
+//     return 0;
+// }
