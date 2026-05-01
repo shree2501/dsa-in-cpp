db3c4a9bd5c71253959bc8582e1d0449bb8bbd55
Shree Sharma <sharmashree750@gmail.com>
2026-05-01 23:01:27 +0530

    Functers

---
+++ Day17/STL_Queue/functers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(){
+    priority_queue <int, vector <int>, greater <int>> q;
+    q.push(1);
+    q.push(2);
+    q.emplace(3);
+    q.pop();
+    cout << q.top() << endl;
+    cout << "Size: " << q.size() << endl;
+    cout << "Is empty: " << q.empty() << endl;
+}
